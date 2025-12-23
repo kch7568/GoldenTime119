24c264bea7be4da44622b1eb00dd6ac0d5dcd804
MintSoda07 <79971109+MintSoda07@users.noreply.github.com>
2025-12-23 11:34:51 +0900

    초기안

---
+++ Source/GoldenTime119/Private/CombustibleType.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombustibleType.h"
+
+CombustibleType::CombustibleType()
+{
+}
+
+CombustibleType::~CombustibleType()
+{
+}
